f7832fb2e34a6b4cae6106de067080974a6fb7a4
jiahui.wen622111@gmail.com <jiahui.wen622111@gmail.com@8360a537-5d4c-cca5-4a4a-2cba06fb616e>
2012-09-01 14:28:15 +0000

    添加mysql驱动， 建立wen测试包。用于个人测试。

---
+++ trunk/Project/src/wen/Connection2MySQL.java
@@ -0,0 +1,18 @@
+package wen;
+
+
+public class Connection2MySQL {
+	
+	/** 
+	 * main:测试连接数据库情况. <br/> 
+	 * 
+	 * @author jiahui.wen 
+	 * @param args 
+	 * @since JDK 1.6 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
