d026d4816093e40e2bde459ee98c1e2b91acaec9
jiahui.wen622111@gmail.com <jiahui.wen622111@gmail.com@8360a537-5d4c-cca5-4a4a-2cba06fb616e>
2012-09-10 11:33:41 +0000

    将setting目录取消跟踪。

--- trunk/YUMMY/YUMMY/src/com/sshmeger/test/TestDAO.java
+++ trunk/YUMMY/YUMMY/src/com/sshmeger/test/TestDAO.java
@@ -8,146 +8,141 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 /**
- 	* A data access object (DAO) providing persistence and search support for Test entities.
- 			* Transaction control of the save(), update() and delete() operations 
-		can directly support Spring container-managed transactions or they can be augmented	to handle user-managed Spring transactions. 
-		Each of these methods provides additional information for how to configure it for the desired type of transaction control. 	
-	 * @see com.sshmeger.test.Test
-  * @author MyEclipse Persistence Tools 
+ * A data access object (DAO) providing persistence and search support for Test
+ * entities. Transaction control of the save(), update() and delete() operations
+ * can directly support Spring container-managed transactions or they can be
+ * augmented to handle user-managed Spring transactions. Each of these methods
+ * provides additional information for how to configure it for the desired type
+ * of transaction control.
+ * 
+ * @see com.sshmeger.test.Test
+ * @author MyEclipse Persistence Tools
  */
 
-public class TestDAO extends HibernateDaoSupport  {
-	     private static final Logger log = LoggerFactory.getLogger(TestDAO.class);
-		//property constants
+public class TestDAO extends HibernateDaoSupport {
+	private static final Logger log = LoggerFactory.getLogger(TestDAO.class);
+	// property constants
 	public static final String NAME = "name";
 	public static final String PASSWORD = "password";
 
-
-
 	protected void initDao() {
-		//do nothing
-	}
-    
-    public void save(Test transientInstance) {
-        log.debug("saving Test instance");
-        try {
-            getHibernateTemplate().save(transientInstance);
-            log.debug("save successful");
-        } catch (RuntimeException re) {
-            log.error("save failed", re);
-            throw re;
-        }
-    }
-    
-	public void delete(Test persistentInstance) {
-        log.debug("deleting Test instance");
-        try {
-            getHibernateTemplate().delete(persistentInstance);
-            log.debug("delete successful");
-        } catch (RuntimeException re) {
-            log.error("delete failed", re);
-            throw re;
-        }
-    }
-    
-    public Test findById( java.lang.String id) {
-        log.debug("getting Test instance with id: " + id);
-        try {
-            Test instance = (Test) getHibernateTemplate()
-                    .get("com.sshmegar.test.Test", id);
-            return instance;
-        } catch (RuntimeException re) {
-            log.error("get failed", re);
-            throw re;
-        }
-    }
-    
-    
-    public List findByExample(Test instance) {
-        log.debug("finding Test instance by example");
-        try {
-            List results = getHibernateTemplate().findByExample(instance);
-            log.debug("find by example successful, result size: " + results.size());
-            return results;
-        } catch (RuntimeException re) {
-            log.error("find by example failed", re);
-            throw re;
-        }
-    }    
-    
-    public List findByProperty(String propertyName, Object value) {
-      log.debug("finding Test instance with property: " + propertyName
-            + ", value: " + value);
-      try {
-         String queryString = "from Test as model where model." 
-         						+ propertyName + "= ?";
-		 return getHibernateTemplate().find(queryString, value);
-      } catch (RuntimeException re) {
-         log.error("find by property name failed", re);
-         throw re;
-      }
+		// do nothing
 	}
 
-	public List findByName(Object name
-	) {
-		return findByProperty(NAME, name
-		);
+	public void save(Test transientInstance) {
+		log.debug("saving Test instance");
+		try {
+			getHibernateTemplate().save(transientInstance);
+			log.debug("save successful");
+		} catch (RuntimeException re) {
+			log.error("save failed", re);
+			throw re;
+		}
 	}
-	
-	public List findByPassword(Object password
-	) {
-		return findByProperty(PASSWORD, password
-		);
+
+	public void delete(Test persistentInstance) {
+		log.debug("deleting Test instance");
+		try {
+			getHibernateTemplate().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
 	}
-	
+
+	public Test findById(java.lang.String id) {
+		log.debug("getting Test instance with id: " + id);
+		try {
+			Test instance = (Test) getHibernateTemplate().get(
+					"com.sshmegar.test.Test", id);
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Test instance) {
+		log.debug("finding Test instance by example");
+		try {
+			List results = getHibernateTemplate().findByExample(instance);
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+
+	public List findByProperty(String propertyName, Object value) {
+		log.debug("finding Test instance with property: " + propertyName
+				+ ", value: " + value);
+		try {
+			String queryString = "from Test as model where model."
+					+ propertyName + "= ?";
+			return getHibernateTemplate().find(queryString, value);
+		} catch (RuntimeException re) {
+			log.error("find by property name failed", re);
+			throw re;
+		}
+	}
+
+	public List findByName(Object name) {
+		return findByProperty(NAME, name);
+	}
+
+	public List findByPassword(Object password) {
+		return findByProperty(PASSWORD, password);
+	}
 
 	public List findAll() {
 		log.debug("finding all Test instances");
 		try {
 			String queryString = "from Test";
-		 	return getHibernateTemplate().find(queryString);
+			return getHibernateTemplate().find(queryString);
 		} catch (RuntimeException re) {
 			log.error("find all failed", re);
 			throw re;
 		}
 	}
-	
-    public Test merge(Test detachedInstance) {
-        log.debug("merging Test instance");
-        try {
-            Test result = (Test) getHibernateTemplate()
-                    .merge(detachedInstance);
-            log.debug("merge successful");
-            return result;
-        } catch (RuntimeException re) {
-            log.error("merge failed", re);
-            throw re;
-        }
-    }
 
-    public void attachDirty(Test instance) {
-        log.debug("attaching dirty Test instance");
-        try {
-            getHibernateTemplate().saveOrUpdate(instance);
-            log.debug("attach successful");
-        } catch (RuntimeException re) {
-            log.error("attach failed", re);
-            throw re;
-        }
-    }
-    
-    public void attachClean(Test instance) {
-        log.debug("attaching clean Test instance");
-        try {
-            getHibernateTemplate().lock(instance, LockMode.NONE);
-            log.debug("attach successful");
-        } catch (RuntimeException re) {
-            log.error("attach failed", re);
-            throw re;
-        }
-    }
+	public Test merge(Test detachedInstance) {
+		log.debug("merging Test instance");
+		try {
+			Test result = (Test) getHibernateTemplate().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Test instance) {
+		log.debug("attaching dirty Test instance");
+		try {
+			getHibernateTemplate().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Test instance) {
+		log.debug("attaching clean Test instance");
+		try {
+			getHibernateTemplate().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
 
 	public static TestDAO getFromApplicationContext(ApplicationContext ctx) {
-    	return (TestDAO) ctx.getBean("TestDAO");
+		return (TestDAO) ctx.getBean("TestDAO");
 	}
 }
