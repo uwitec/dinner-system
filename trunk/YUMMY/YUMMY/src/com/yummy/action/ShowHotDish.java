fdde1c5483d854aa7777f686ecbecc5c62a38dad
jiahui.wen622111@gmail.com <jiahui.wen622111@gmail.com@8360a537-5d4c-cca5-4a4a-2cba06fb616e>
2012-09-18 13:00:36 +0000

    配置显示热门食物的页面。

---
+++ trunk/YUMMY/YUMMY/src/com/yummy/action/ShowHotDish.java
@@ -0,0 +1,22 @@
+package com.yummy.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * ClassName: ShowHotDish <br/> 
+ * Function: 用户访问主页面时显示热门的食物. <br/> 
+ * date: 2012-9-18 下午8:31:39 <br/> 
+ * 
+ * @author jiahui 
+ * @version 1.0
+ */
+public class ShowHotDish extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+	
+}
