4b2fed17b86cb9547092d034d3209945a5a26d09
jiahui.wen622111@gmail.com <jiahui.wen622111@gmail.com@8360a537-5d4c-cca5-4a4a-2cba06fb616e>
2012-09-21 16:24:54 +0000

    提交建表语句修改版

--- trunk/YUMMY/YUMMY/WebRoot/db/整体建表语句修改版.sql
+++ trunk/YUMMY/YUMMY/WebRoot/db/整体建表语句修改版.sql
@@ -1,125 +1,8 @@
-
--- ----------------------------
--- Table structure for `customer` 用户表
--- ----------------------------
 SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE `customer` (
-  `username` varchar(15) NOT NULL DEFAULT '',
-  `password` varchar(15) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `gender` varchar(2) NOT NULL,
-  `credits` int(10) unsigned zerofill NOT NULL DEFAULT '0000000000',
-  `register_date` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`username`,`email`),
-  KEY `index_customer` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of customer
+-- Table structure for `customer_info`
 -- ----------------------------
-
--- ----------------------------
--- Table structure for `shop` 店铺表
--- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS `shop`;
-CREATE TABLE `shop` (
-  `shopname` varchar(15) NOT NULL DEFAULT '',
-  `address` varchar(30) NOT NULL,
-  `telephone` varchar(15) NOT NULL,
-  `qq` varchar(12) DEFAULT NULL,
-  `introduction` varchar(50) DEFAULT NULL,
-  `logo` longblob NOT NULL,
-  `point` int(10) NOT NULL DEFAULT '0' COMMENT '商家的信誉，以分数形式出现',
-  `delivery_range` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`shopname`),
-  UNIQUE KEY `index_shopname` (`shopname`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of shop
--- ----------------------------
-
--- ----------------------------
--- Table structure for `dish` 食物表
--- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS `dish`;
-CREATE TABLE `dish` (
-  `dishID` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(15) NOT NULL,
-  `point` int(11) DEFAULT NULL COMMENT '每种食物评价后都可以得到一定分数',
-  `price` float NOT NULL,
-  `shopname` varchar(15) NOT NULL,
-  `introduction` varchar(50) DEFAULT NULL,
-  `category` varchar(10) DEFAULT NULL COMMENT '食物的分类',
-  `tag` varchar(10) DEFAULT NULL COMMENT '自定义的标签',
-  `upload_time` date DEFAULT NULL COMMENT '上传时间',
-  `prcOne` blob
-  `prcTwo` blob
-  `prcThree` blob
-  PRIMARY KEY (`dishID`),
-  KEY `fk_dish_shop` (`shopname`),
-  CONSTRAINT `fk_dish_shop` FOREIGN KEY (`shopname`) REFERENCES `shop` (`shopname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of dish
--- ----------------------------
-
--- ----------------------------
--- Table structure for `collect_dish` 收藏食物表
--- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS `collect_dish`;
-CREATE TABLE `collect_dish` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `username` varchar(15) NOT NULL,
-  `dishid` int(10) NOT NULL,
-  `collect_time` date NOT NULL,
-  `tag` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_collect_customer2` (`username`),
-  KEY `fk_collect_dish` (`dishid`),
-  CONSTRAINT `fk_collect_customer2` FOREIGN KEY (`username`) REFERENCES `customer` (`username`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_collect_dish` FOREIGN KEY (`dishid`) REFERENCES `dish` (`dishID`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of collect_dish
--- ----------------------------
-
-
-
--- ----------------------------
--- Table structure for `collect_shop`收藏店铺表
--- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS `collect_shop`;
-CREATE TABLE `collect_shop` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `username` varchar(15) NOT NULL,
-  `shopname` varchar(15) NOT NULL,
-  `collect_time` date NOT NULL,
-  `tag` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_collect_customer` (`username`),
-  KEY `fk_collect_shop` (`shopname`),
-  CONSTRAINT `fk_collect_customer` FOREIGN KEY (`username`) REFERENCES `customer` (`username`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_collect_shop` FOREIGN KEY (`shopname`) REFERENCES `shop` (`shopname`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of collect_shop
--- ----------------------------
-
-
-
--- ----------------------------
--- Table structure for `customer_info` 用户信息表，包括地址电话等
--- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `customer_info`;
 CREATE TABLE `customer_info` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
@@ -135,19 +18,108 @@ CREATE TABLE `customer_info` (
 -- Records of customer_info
 -- ----------------------------
 
+-- ----------------------------
+-- Table structure for `shop`
+-- ----------------------------
+DROP TABLE IF EXISTS `shop`;
+CREATE TABLE `shop` (
+  `shopname` varchar(15) NOT NULL DEFAULT '',
+  `address` varchar(50) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  `qq` varchar(12) DEFAULT NULL,
+  `introduction` varchar(100) DEFAULT NULL,
+  `logo` longblob NOT NULL,
+  `point` int(10) NOT NULL DEFAULT '0' ,
+  `delivery_range` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`shopname`),
+  UNIQUE KEY `index_shopname` (`shopname`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `dish`
+-- ----------------------------
+DROP TABLE IF EXISTS `dish`;
+CREATE TABLE `dish` (
+  `dishID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) NOT NULL,
+  `point` int(11) unsigned zerofill DEFAULT '00000000000' ,
+  `price` float NOT NULL,
+  `shopname` varchar(15) NOT NULL,
+  `introduction` varchar(100) DEFAULT NULL,
+  `category` varchar(10) DEFAULT NULL ,
+  `tag` varchar(10) DEFAULT NULL ,
+  `upload_time` date DEFAULT NULL ,
+  `picOne` longblob NOT NULL,
+  `picTwo` longblob,
+  `picThree` longblob,
+  PRIMARY KEY (`dishID`),
+  KEY `fk_dish_shop` (`shopname`),
+  CONSTRAINT `fk_dish_shop` FOREIGN KEY (`shopname`) REFERENCES `shop` (`shopname`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 
 -- ----------------------------
--- Table structure for `estimate` 评价表
+-- Table structure for `collect_dish`
 -- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS `collect_dish`;
+CREATE TABLE `collect_dish` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(15) NOT NULL,
+  `dishid` int(10) NOT NULL,
+  `collect_time` date DEFAULT NULL,
+  `tag` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_collect_customer2` (`username`),
+  KEY `fk_collect_dish` (`dishid`),
+  CONSTRAINT `fk_collect_customer2` FOREIGN KEY (`username`) REFERENCES `customer` (`username`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_collect_dish` FOREIGN KEY (`dishid`) REFERENCES `dish` (`dishID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `collect_shop`
+-- ----------------------------
+DROP TABLE IF EXISTS `collect_shop`;
+CREATE TABLE `collect_shop` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(15) NOT NULL,
+  `shopname` varchar(15) NOT NULL,
+  `collect_time` date NOT NULL,
+  `tag` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_collect_customer` (`username`),
+  KEY `fk_collect_shop` (`shopname`),
+  CONSTRAINT `fk_collect_customer` FOREIGN KEY (`username`) REFERENCES `customer` (`username`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_collect_shop` FOREIGN KEY (`shopname`) REFERENCES `shop` (`shopname`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `customer_info`
+-- ----------------------------
+DROP TABLE IF EXISTS `customer_info`;
+CREATE TABLE `customer_info` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(15) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  `address` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_01` (`username`),
+  CONSTRAINT `FK_01` FOREIGN KEY (`username`) REFERENCES `customer` (`username`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of customer_info
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `estimate`
+-- ----------------------------
 DROP TABLE IF EXISTS `estimate`;
 CREATE TABLE `estimate` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `username` varchar(15) NOT NULL,
   `dish_id` int(10) NOT NULL,
-  `point` smallint(6) NOT NULL DEFAULT '5' COMMENT '给每种食物评价分数',
-  `comment` longblob,
+  `point` smallint(6) NOT NULL DEFAULT '5' ,
+  `comment` varchar(100) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_estimate_dish` (`dish_id`),
@@ -157,21 +129,26 @@ CREATE TABLE `estimate` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of estimate
+-- Table structure for `gift`
 -- ----------------------------
-
-
+DROP TABLE IF EXISTS `gift`;
+CREATE TABLE `gift` (
+  `id` int(10) NOT NULL DEFAULT '0',
+  `gift_name` varchar(20) NOT NULL,
+  `grade` int(5) NOT NULL ,
+  `account` int(5) NOT NULL ,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for `gift_exchange` 兑换礼品表
+-- Table structure for `gift_exchange`
 -- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `gift_exchange`;
 CREATE TABLE `gift_exchange` (
   `id` int(10) NOT NULL,
   `username` varchar(15) NOT NULL,
   `gift_id` int(10) NOT NULL,
-  `date` date DEFAULT NULL COMMENT '礼品换购时间',
+  `date` date DEFAULT NULL ,
   PRIMARY KEY (`id`),
   KEY `fk_gift` (`gift_id`),
   KEY `fk_gift_customer` (`username`),
@@ -180,39 +157,14 @@ CREATE TABLE `gift_exchange` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of gift_exchange
+-- Table structure for `order`
 -- ----------------------------
-
-
--- ----------------------------
--- Table structure for `gift` 礼品表
--- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS `gift`;
-CREATE TABLE `gift` (
-  `id` int(10) NOT NULL DEFAULT '0',
-  `gift_name` varchar(20) NOT NULL,
-  `grade` int(5) NOT NULL COMMENT '换购该礼品所需分数',
-  `account` int(5) NOT NULL COMMENT '该礼品的数量',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of gift
--- ----------------------------
-
-
-
--- ----------------------------
--- Table structure for `order` 订单表
--- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order` (
   `order_id` int(10) NOT NULL AUTO_INCREMENT,
   `order_time` datetime NOT NULL,
-  `status` int(1) NOT NULL COMMENT '订单的状态',
-  `user_info` int(10) NOT NULL COMMENT '通过这里找到名字，地址，电话.',
+  `status` int(1) NOT NULL ,
+  `user_info` int(10) NOT NULL ,
   `total_price` float DEFAULT NULL,
   `message` varchar(50) DEFAULT NULL,
   `grade` int(11) NOT NULL,
@@ -222,14 +174,7 @@ CREATE TABLE `order` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of order
--- ----------------------------
-
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for `order_info` 订单信息表
+-- Table structure for `order_info`
 -- ----------------------------
 DROP TABLE IF EXISTS `order_info`;
 CREATE TABLE `order_info` (
@@ -245,15 +190,8 @@ CREATE TABLE `order_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of order_info
+-- Table structure for `hot_food`
 -- ----------------------------
-
-
-
--- ----------------------------
--- Table structure for `hot_food` 促销食物表
--- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `hot_food`;
 CREATE TABLE `hot_food` (
   `id` int(10) NOT NULL,
@@ -261,7 +199,7 @@ CREATE TABLE `hot_food` (
   `shop_id` varchar(15) NOT NULL,
   `current_price` float NOT NULL,
   `account` int(5) NOT NULL,
-  `valid_date` date DEFAULT NULL COMMENT '促销时间',
+  `valid_date` date DEFAULT NULL ,
   PRIMARY KEY (`id`),
   KEY `fk_hotfood_dish` (`dish_id`),
   KEY `fk_hotfood_shop` (`shop_id`),
@@ -270,15 +208,8 @@ CREATE TABLE `hot_food` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of hot_food
+-- Table structure for `shop_worker`
 -- ----------------------------
-
-
-
--- ----------------------------
--- Table structure for `shop_worker` 店铺工作人员表
--- ----------------------------
-SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `shop_worker`;
 CREATE TABLE `shop_worker` (
   `username` varchar(15) NOT NULL DEFAULT '',
@@ -290,14 +221,6 @@ CREATE TABLE `shop_worker` (
   KEY `fk_worker_shop` (`shopname`),
   CONSTRAINT `fk_worker_shop` FOREIGN KEY (`shopname`) REFERENCES `shop` (`shopname`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of shop_worker
--- ----------------------------
-
-
-
-
 
 
 
